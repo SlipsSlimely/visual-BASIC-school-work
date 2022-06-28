81307a62f3e857d48d41beb01dfc26f37b18f14a
SlipsSlimely <107329067+SlipsSlimely@users.noreply.github.com>
2022-06-28 14:51:04 -0400

    Add files via upload
    Main forms and code bodies from all of my other visual BASIC assignments

--- Main Form.vb
+++ Main Form.vb
@@ -1,57 +1,106 @@
-﻿' Project name:         Calculator Project
-' Project purpose:      Display the sum of the numbers entered
-' Created/revised by:   <Paul Bahdouchi> on <10/30/2017>
+﻿Public Class Form1
 
-Option Explicit On
-Option Strict On
-Option Infer Off
-
-Public Class MainForm
-
-    Dim accumulator As Double
-    Dim numbers As Double
+    Dim ProcessingFee As Double
+    Dim Premium As Double
+    Dim BasicFee As Double
+    Dim Total As Double
+    Dim channels As String
+    Dim connection As String
 
 
-    ' this makes the exit button close the application
-    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+    'Public Shared Function ToDouble(channels As String) As Double
+    'End Function
+
+    Public Shared Function ToDouble(connection As String) As Double
+    End Function
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs
+   ) Handles Me.Load
+
+        ' puts stuff in the list box
+        ConnectionListBox.Items.Add("3")
+        ConnectionListBox.Items.Add("4")
+        ConnectionListBox.Items.Add("5")
+        ConnectionListBox.Items.Add("6")
+        ConnectionListBox.Items.Add("7")
+        ConnectionListBox.Items.Add("8")
+        ConnectionListBox.Items.Add("9")
+        ConnectionListBox.Items.Add("10")
+        ConnectionListBox.Items.Add("11")
+        ConnectionListBox.Items.Add("12")
+        ConnectionListBox.Items.Add("13")
+        ConnectionListBox.Items.Add("14")
+        ConnectionListBox.Items.Add("15")
+        ConnectionListBox.Items.Add("16")
+        ConnectionListBox.Items.Add("17")
+        ConnectionListBox.Items.Add("18")
+        ConnectionListBox.Items.Add("19")
+        ConnectionListBox.Items.Add("20")
+        ConnectionListBox.SelectedIndex = 0
+
+        PremiumListBox.Items.Add("0")
+        PremiumListBox.Items.Add("1")
+        PremiumListBox.Items.Add("2")
+        PremiumListBox.Items.Add("3")
+        PremiumListBox.Items.Add("4")
+        PremiumListBox.Items.Add("5")
+        PremiumListBox.Items.Add("6")
+        PremiumListBox.Items.Add("7")
+        PremiumListBox.Items.Add("8")
+        PremiumListBox.Items.Add("9")
+        PremiumListBox.Items.Add("10")
+        PremiumListBox.Items.Add("11")
+        PremiumListBox.Items.Add("12")
+        PremiumListBox.Items.Add("13")
+        PremiumListBox.Items.Add("14")
+        PremiumListBox.Items.Add("15")
+        PremiumListBox.Items.Add("16")
+        PremiumListBox.Items.Add("17")
+        PremiumListBox.Items.Add("18")
+        PremiumListBox.Items.Add("19")
+        PremiumListBox.Items.Add("20")
+        PremiumListBox.SelectedIndex = 0
+
+        connection = ConnectionListBox.Items().ToString
+
+        channels = PremiumListBox.Items().ToString
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        ProcessingFee = 4.5
+        Premium = 5
+        BasicFee = 30
+    End Sub
+
+    Private Sub PremiumListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles PremiumListBox.SelectedIndexChanged
+        
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        ProcessingFee = 16.5
+        Premium = 50
+        BasicFee = 80
+    End Sub
+
+    ' closes the app
+    Private Sub exitButton_Click(sender As Object, e As EventArgs
+    ) Handles exitButton.Click
         Me.Close()
     End Sub
 
-    Private Sub numTextBox_KeyPress(sender As Object, e As KeyPressEventArgs) Handles numTextBox.KeyPress
-        ' allow the text box to accept only numbers and the Backspace
+    Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles calcButton.Click
 
-        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso e.KeyChar <> ControlChars.Back Then
-            e.Handled = True
+        If RadioButton2.Checked Then
+            Total = ProcessingFee + (Premium * channels) + BasicFee
+        Else
+            Total = ProcessingFee + (Premium * channels) + BasicFee
+
         End If
-    End Sub
 
-    ' this makes the add button add things
-    Private Sub addButton_Click(sender As Object, e As EventArgs) Handles addButton.Click
-
-        ' this pulls the text in numtextbox to the numbers variable
-        Double.TryParse(numTextBox.Text, numbers)
-
-        ' this adds the number variable to the accumulator
-        accumulator = accumulator + numbers
-
-        ' this displays the numbers in the appropriate boxes
-        numbersTextBox.Text = numbersTextBox.Text &
-        numbers.ToString & ControlChars.NewLine
-
-
-        ' this displays the accumulator in the sumLabel text box
-        sumLabel.Text = accumulator.ToString
+        TotalDueLabel.Text = Total.ToString()
 
     End Sub
 
-    ' this makes the start over button reset the app
-    Private Sub startOverButton_Click(sender As Object, e As EventArgs) Handles startOverButton.Click
-
-        numbersTextBox.Text = String.Empty
-        sumLabel.Text = String.Empty
-        accumulator = 0
-        numbers = 0
-
-    End Sub
 
 End Class
